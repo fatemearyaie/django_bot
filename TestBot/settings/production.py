3a7d59404a80cc64011761aa48213c1711591913
fateme <fatemearyaie9@gmail.com>
2026-02-11 11:27:29 +0330

    production setting

---
+++ TestBot/settings/production.py
@@ -0,0 +1,42 @@
+import os
+from decouple import config
+from .base import *
+from corsheaders.defaults import default_headers
+
+ALLOWED_HOSTS=['84.200.24.159','api.hayari.app','hayari.app','localhost','127.0.0.1','0.0.0.0']
+
+
+DEBUG = config("DEBUG")
+SECRET_KEY = config("SECRET_KEY")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("POSTGRES_DB"),
+        "USER":config("POSTGRES_USER"),
+        "PASSWORD":config("POSTGRES_PASSWORD"),
+        "HOST":config("POSTGRES_HOST"),
+        "PORT":config("POSTGRES_PORT","5432"),
+    }
+}
+
+
+
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGIN = [
+    'https://api.excoinmarket.shop',
+]
+CORS_ALLOW_HEADERS = list(default_headers) + ['x-csrftoken']
+
+
+# --- Security hardening ---
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
