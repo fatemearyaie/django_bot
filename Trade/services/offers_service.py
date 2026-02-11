50d692429a7cbaa43cc3305d4b30e8cbe0a1d5ad
fateme <fatemearyaie9@gmail.com>
2026-02-11 17:22:49 +0330

    update username

--- Trade/services/offers_service.py
+++ Trade/services/offers_service.py
@@ -4,13 +4,7 @@ from telegram import Bot, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.error import TelegramError
 from Trade.models.models import TradeRequest
 
-BOT_USERNAME = None
-
-def init_bot_username():
-    global BOT_USERNAME
-    token = os.environ["API_TOKEN"]
-    me = async_to_sync(Bot(token=token).get_me)()
-    BOT_USERNAME = me.username
+BOT_USERNAME = "excoinmarket_bot"
 
 
 def build_offer_manage_keyboard(offer_id: int) -> InlineKeyboardMarkup:
