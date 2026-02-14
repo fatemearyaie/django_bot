d3ad0c0739f2c5134880c4e555f6619474cb6984
fateme <fatemearyaie9@gmail.com>
2026-02-14 11:44:11 +0330

    unfold rtl updated

--- TestBot/settings/base.py
+++ TestBot/settings/base.py
@@ -1,4 +1,3 @@
-
 import os
 from datetime import timedelta
 from pathlib import Path
@@ -12,9 +11,7 @@ from django.urls import reverse_lazy
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -34,6 +31,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -61,7 +59,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'TestBot.wsgi.application'
 
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -77,33 +74,34 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 LANGUAGE_CODE = 'fa'
+LANGUAGES = [
+    ('fa', _('Persian')),
+    ('en', _('English')),
+]
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
 TIME_ZONE = 'Asia/Tehran'
-
-
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
-
 AUTH_USER_MODEL = 'Users.CustomUser'
 
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
 
 UNFOLD = {
     "DASHBOARD_CALLBACK": "TestBot.admin.dashboard.dashboard_context",
     "SCRIPTS": [lambda request: static("admin/js/chart.umd.min.js")],
-    "STYLES": [lambda request: static("admin/css/rtl-actions-fix.css"),],
-
-
+    "STYLES": [
+        lambda request: static("css/persian-admin.css"),
+    ],
 
     "SITE_TITLE": "ExCoinMarket",
     "SITE_HEADER": "ExCoinMarket",
@@ -112,7 +110,6 @@ UNFOLD = {
     "THEME": None,
     "SHOW_HISTORY": True,
     "SHOW_VIEW_ON_SITE": True,
-
 
     "SIDEBAR": {
         "show_search": True,
