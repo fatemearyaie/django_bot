4fcb31100cfc6274812a0c3a12054e6c817f4469
fateme <fatemearyaie9@gmail.com>
2026-02-21 17:07:18 +0330

    test requests offer DEBUG

--- bot/flow/offer_manager.py
+++ bot/flow/offer_manager.py
@@ -57,12 +57,14 @@ async def offer_accept_cb(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         await context.bot.send_message(
             chat_id=offer.sender.telegram_id,
-            text="\n\n✅ *پیشنهاد شما تایید شد.*"
-                f"\nاین پیام رو برای ادمین بفرستید تا ارتباط بین شما و درخواست دهنده برقرار بشه. "
+            text=
+            "✅ *پیشنهاد شما تایید شد*\n\n"
                 "🧾 پیش‌نمایش پیشنهاد شما:\n\n"
                 f"📌 آگهی: {offer.request.id}\n"
-                f"✅ نرخ پیشنهادی شما (تومان/واحد): {offer.unit_price_irt}\n"
-                f"📝 توضیحات: {offer.message if offer.message else '—'}\n"
+                f"✅ نرخ پیشنهادی شما: {offer.unit_price_irt}\n"
+                f"📝 توضیحات: {offer.message if offer.message else '—'}\n\n\n"
+                f"\nاین پیام رو برای ادمین بفرستید تا ارتباط بین شما و درخواست دهنده برقرار بشه. "
+
         )
     except Exception:
         pass
